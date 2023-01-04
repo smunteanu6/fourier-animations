570e7246e037d0cdc6bf676403720918922b9400
Stefan Munteanu <smunteanu0206@gmail.com>
2023-01-04 16:56:47 +0200

    Introduced math module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
-class Line {
-  
-  constructor (x1, y1, x2, y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-    this.m = (y1 - y2) / (x1 - x2);
-    this.b = y1 - x1 * this.m;
-    this.length = Math.sqrt((this.x1 - this.x2) ** 2 + (this.y1 - this.y2) ** 2);
-  }
-
-  // The point at distance d from (x1, y1), directed to (x2, y2)
-  atDistance(d) {
-    if (this.x1 == this.x2) return [this.x1, this.y1 + (this.y1 < this.y2 ? d : -d)];
-    // w = change in x, h = change in y
-    // w^2+h^2=d^2   =>   w^2+(mw)^2-d^2   =   (m^2+1)*w^2-d^2   =   0
-    // To solve for w: a = m^2+1, b = 0, c = -d^2
-    // In this case, w = (-b+sqrt(b^2-4ac))/2a   =   sqrt(-4ac)/2a   =   sqrt(4ad^2)/2a   =   2d*sqrt(a)/2a   =   d/sqrt(a) =   d/sqrt(m^2+1)
-    const x = this.x1 + (d / Math.sqrt(this.m * this.m + 1)) * (this.x1 < this.x2 ? 1 : -1);
-    return [x, this.m * x + this.b];
-  }
-
-}
+const math = require('./math');
 
 class fImage {
 
@@ -119,7 +96,7 @@ const main = (async () => {
 
   const linesData = [[4, 4, 200, 200], [200, 200, 280, 160], [280, 160, 4, 4]];
 
-  const lines = linesData.map(data => new Line(data[0], data[1], data[2], data[3]));
+  const lines = linesData.map(data => new math.Segment(data[0], data[1], data[2], data[3]));
 
   const image = new fImage(size, lines);
 
